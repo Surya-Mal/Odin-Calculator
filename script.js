1e59e561eea37d6a6752e4fafc2c508080958193
Surya Mallidi <surya_mallidi@outlook.com>
2025-01-14 22:03:15 -0600

    added fucntionalies including the ability to clear the input and displaying the result of an opertion on the display between 2 numbers

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let op = 0;
-let op2 = 0;
+let op = "";
+let op2 = "";
 let action = "";
 
 function add(num1, num2){
@@ -19,26 +19,62 @@ function divide(num1, num2){
 }
 
 function operator(op, num1, num2){
+    let finalValue;
     switch(op){
         case "+": 
-            add(num1, num2) 
+            finalValue = add(num1, num2) 
             break;
         case "-":
-            subtract(num1, num2)
+            finalValue = subtract(num1, num2)
             break;
         case "*":
-            multiply(num1, num2)
+            finalValue = multiply(num1, num2)
             break;
         case "/":
-            divide(num1, num2)
+            finalValue = divide(num1, num2)
             break
     }
+    return finalValue;
 }
 
-const nums = document.querySelectorAll(".numbers");
+const nums = document.querySelectorAll(".clickable");
 const input = document.querySelector("input")
 nums.forEach((btn) => {
     btn.addEventListener("click", () => {
         input.value += btn.textContent //The value attribute either sets or returns value of an attribute
+        if(Number.isInteger(Number(btn.textContent)) && action == ""){
+            op += btn.textContent 
+            console.log(op);
+        }
+        else if(action != "" && Number.isInteger(Number(btn.textContent))){
+            op2 += btn.textContent
+            console.log(op2)
+        }
     })
+})
+
+const ops = document.querySelectorAll(".ops")
+ops.forEach((btn) => {
+    btn.addEventListener("click", () =>{
+        input.value += btn.textContent;
+        op = Number(op);
+        action = btn.textContent;
+    })
+})
+
+const final = document.querySelector("#finish");
+final.addEventListener("click", () =>{
+    op2 = Number(op2);
+    console.log(op)
+    console.log(op2)
+    console.log(action)
+    input.value = operator(action, op, op2);
+})
+
+const erase = document.querySelector("#erase");
+erase.addEventListener("click", () => {
+    input.value = "";
+    op = ""
+    op2 = ""
+    action = ""
 })
